c2f3cfe7096c25a8d0dfd9c279d7e6f73bf17d0a
euan jones <euandmj97@gmail.com>
2020-05-11 23:03:53 +0100

    how do bindings work again???

---
+++ fileCrawlerWPF/Ctrls/ctlFileInformation.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace fileCrawlerWPF.Ctrls
+{
+    /// <summary>
+    /// Interaction logic for ctlFileInformation.xaml
+    /// </summary>
+    public partial class ctlFileInformation : UserControl
+    {
+        public ProbeFile ProbeFile { get; set; }
+        public static readonly DependencyProperty ProbeFileDependencyProperty =
+            DependencyProperty.Register("ProbeFile", typeof(ProbeFile), typeof(ctlFileInformation), new PropertyMetadata());
+
+        public ctlFileInformation()
+        {
+            InitializeComponent();
+        }
+
+
+        public string FileName { get { return ProbeFile.Name; } set { } }
+
+        private void TextBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
